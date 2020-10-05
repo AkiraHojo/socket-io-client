9c67f535a29f4327a514011354bb8c2c1515432b
Akira Hojo <akira.h.good@gmail.com>
2020-10-05 20:38:33 +0900

    fixed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
-// import Example from "./chartU";
+ 
+import Example from "./chartU";
 const ENDPOINT = "http://192.168.0.16:4001";
 
 function App() {
-  const [response, setResponse] = useState("");
   const [count, setCount] = useState(0);
   const [dataarray, setDataarray] = useState([]);
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     socket.on("FromAPI", data => {
-      setResponse(data);
       setDataarray((prevDataarray) => {
         prevDataarray[prevDataarray.length] = data;
         return prevDataarray;
@@ -26,12 +25,8 @@ function App() {
 
   return (
     <p>
-      <div>{response}</div>
       <div>{count}</div>
-      <div>{dataarray}</div>
-      {/* <div>{dataarray.map(value => value + ",")}</div> */}
-      {/* <Example value={dataarray.map((val, index) => {return {name: index , value: val};})}/> */}
-      {/* <Example value={response}/> */}
+      <Example value={dataarray.map((val, index) => {return {name: index , value: val};})}/>
     </p>
   );
 }

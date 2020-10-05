726e04b94806b5a66b122944aa12760a20081fd4
Akira Hojo <akira.h.good@gmail.com>
2020-10-05 15:30:58 +0900

    Success Dataaraay

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ const ENDPOINT = "http://192.168.0.16:4001";
 function App() {
   const [response, setResponse] = useState("");
   const [count, setCount] = useState(0);
+  const [dataarray, setDataarray] = useState([]);
 
   useEffect(() => {
     const socket = socketIOClient(ENDPOINT);
     socket.on("FromAPI", data => {
       setResponse(data);
+      setDataarray((prevDataarray) => {
+        prevDataarray[prevDataarray.length] = data;
+        return prevDataarray;
+      });
       setCount(prevCount => prevCount + 1);
     });
 
@@ -24,6 +29,7 @@ function App() {
       {/* It's <time dateTime={response}>{response}</time> */}
       <div>{response}</div>
       <div>{count}</div>
+      <div>{dataarray.map(value => value + ",")}</div>
       <Example value={response}/>
       {/* <button onClick={() => {setCount(count+1)}}>button</button> */}
     </p>
